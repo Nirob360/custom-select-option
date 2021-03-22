d5044d7587a83444fe564e361880af695f298ff5
Nirob360 <mdminarulislam944@gmail.com>
2021-03-22 22:08:51 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const selected = document.querySelector('.slected');
+const optionContainer = document.querySelector('.options-container');
+const optionList = document.querySelectorAll('.option');
+
+selected.addEventListener('click', () => {
+
+    optionContainer.classList.toggle('active')
+})
+optionList.forEach(o => {
+    o.addEventListener('click', () => {
+        selected.innerHTML = o.querySelector('label').innerHTML;
+        optionContainer.classList.remove('active');
+    })
+})
